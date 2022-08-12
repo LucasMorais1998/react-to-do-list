32cc07ddcb2b7ec6fc2740838174a9cef2785923
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-11 23:04:06 -0300

    add Header component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
+import Header from '../../components/Header';
 import { Container, MainContent } from './style'
 
 const Home = () => {
   return (
     <Container>
       <MainContent>
-        ...
+        <Header/>
       </MainContent>
     </Container>
   );
